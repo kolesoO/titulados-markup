289ed8c5f4a236ba8bb33266386b555e5151de50
Ada Kolesnichenko <adakolesnichenko@MacBook-Air-Ada.sknt.ru>
2020-06-04 18:10:13 +0300

    order-list + chat

--- new/js/index.js
+++ new/js/index.js
@@ -1,5 +1,21 @@
 import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
-
 $(document).ready(function() {
   obSlider.init();
+
+  $( "body" ).on("click", ".js-toggle-down", function() {
+    let target = $(this).parents(".js-toggle-wrapper").first().find(".js-toggle-target");
+    target.slideToggle( "fast");
+      if (target.css("display") !== "none") {
+        $(this).addClass("d-none");
+        $(this).removeClass("d-flex");
+      }
+  });
+  $( "body" ).on("click", ".js-toggle-up", function() {
+    let target = $(this).parents(".js-toggle-wrapper").first().find(".js-toggle-target");
+    target.slideToggle( "fast" );
+    $(this).parents(".js-toggle-wrapper").first().find(".js-toggle-down")
+      .addClass("d-flex")
+      .removeClass("d-none");
+  });
+
 });
